42fbc194229e24952101896f01855fc77600898f
JeanThomaz <jeanlucas.tjc@gmail.com>
2022-10-14 21:45:07 -0300

    Modulo 2 alteração

--- modulo2/src/Comprar.java
+++
@@ -1,5 +0,0 @@
-public interface Comprar {
-
-    public void comprar();
-
-}
--- modulo2/src/EmailRepetidoException.java
+++
@@ -1,6 +0,0 @@
-public class EmailRepetidoException extends Exception {
-
-    public EmailRepetidoException(String mensagem){
-        super(mensagem);
-    }
-}
